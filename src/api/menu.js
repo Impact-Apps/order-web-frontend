dac447536950eeee16cb995004dadd5dead90960
Russell Brady <russell.brady29@mail.dcu.ie>
2020-04-02 20:47:25 +0100

    Adding testing frameworks, axios request client and tests, apis and tests

---
+++ src/api/menu.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+import API_CONSTANTS from '@/constants/api'
+
+const postMenu = async payload => request.create(API_CONSTANTS.MENU, payload)
+
+const getMenu = async id => request.fetch(`${API_CONSTANTS.MENU}/${id}`)
+
+const deleteMenu = async id => request.delete(API_CONSTANTS.MENU, id)
+
+export default { postMenu, getMenu, deleteMenu }
---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+const endPoints = {
+  BASE_URL: '',
+  TIMEOUT: 120000,
+  MENU: '/api/menu',
+  RESTAURANT: '/api/restaurant',
+  ITEM: '/api/item'
+}
+export default endPoints
