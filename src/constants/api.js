1799c972edc2260093b051af210714a32bc29f31
Russell Brady <russell.brady29@mail.dcu.ie>
2020-04-22 21:46:05 +0100

    Adding restaurant and item review service and api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,14 @@
 const endPoints = {
   BASE_URL: 'http://localhost:3003',
   TIMEOUT: 120000,
+
+  //ENDPOINTS
   MENU: '/api/menu',
   RESTAURANT: '/api/restaurant',
   ITEM: '/api/item',
-  ORDER: '/api/order'
+  ORDER: '/api/order',
+
+  //REVIEWS
+  REVIEWS: '/reviews'
 }
 export default endPoints
